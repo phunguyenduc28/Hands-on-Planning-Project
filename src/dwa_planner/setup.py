67819e6d70d37e55460a63bb9e31aea7d08fff28
Mohammad Haadi Akhter <haadiakhter@gmail.com>
2026-05-11 17:47:26 +0200

    dwa_planner package

---
+++ src/dwa_planner/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'dwa_planner'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='huy',
+    maintainer_email='dohuy9379@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'control_tb = dwa_planner.control_tb:main',
+            'occupancy_grid_original = dwa_planner.occupancy_grid_original:main',
+        ],
+    },
+)
